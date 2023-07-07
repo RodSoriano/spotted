56eea0293428c3af118e19971770dd53e02f7dfa
RodSoriano <rsoriano.jose@gmail.com>
2023-07-06 19:46:55 -0600

    Create basic factory for user model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,25 +18,16 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'role_id' => fake()->numberBetween(1, 2),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => fake()->email(),
+            'date_of_birth' => fake()->date(),
+            'photo' => fake()->random_bytes(5),
+            'emergency_contact' => fake()->name(),
+            'emergency_contact_number' => fake()->phoneNumber(),
+            'status' => fake()->randomElement(['green', 'yellow', 'red']),
+            'fee' => fake()->randomElement([2, 5, 10]),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
