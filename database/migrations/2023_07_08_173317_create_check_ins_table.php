78efdd3dee83333da993cde0ca5f00f17605ffc6
RodSoriano <rsoriano.jose@gmail.com>
2023-07-08 11:57:03 -0600

    Create check in migration

---
+++ database/migrations/2023_07_08_173317_create_check_ins_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('check_ins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->boolean('promo_code');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('check_ins');
+    }
+};
