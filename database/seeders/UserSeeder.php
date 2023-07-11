92f8c3a738222e29f450ac14da5056d8e3b2a641
RodSoriano <rsoriano.jose@gmail.com>
2023-07-10 18:32:52 -0600

    Create user seeder for admins and users.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\UserRole;
+use App\Models\Admin;
+use App\Models\CheckIn;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Admin::factory(2)
+            ->create([
+                'role_id' => UserRole::ADMIN->value,
+            ]);
+
+        User::factory(10)
+            ->has(CheckIn::factory())
+            ->create([
+                'role_id' => UserRole::USER->value,
+            ]);
+    }
+}
