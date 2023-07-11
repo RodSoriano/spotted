3794569e3eba9d34fdce82eb185e328054e84bb2
RodSoriano <rsoriano.jose@gmail.com>
2023-07-10 18:02:06 -0600

    Create values function to return all cases in order.

--- app/Enum/UserStatus.php
+++ app/Enum/UserStatus.php
@@ -7,4 +7,11 @@ enum UserStatus: string
     case ACCEPTED = 'Accepted User';
     case WARNED = 'Warned User';
     case BANNED = 'Banned User';
+
+    public static function values(): array
+    {
+        return array_map(function ($enum) {
+            return $enum->value;
+        }, self::cases());
+    }
 }
