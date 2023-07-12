c8650a964f4655e1765a8826e6aa05dd533543b2
RodSoriano <rsoriano.jose@gmail.com>
2023-07-12 11:53:31 -0600

    Create app component using react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
+    root.render(<App {...props} />);
+  }
+});
