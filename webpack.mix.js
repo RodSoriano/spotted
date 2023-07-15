1779ae5a56c427be7c07968815df7daf0f3a3c51
RodSoriano <rsoriano.jose@gmail.com>
2023-07-15 16:29:39 -0600

    Compile web-pack.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js/app.js').css('resources/css/app.css', 'public/css/app.css');
+mix.js('resources/js/app.js', 'public/js/app.js')
+  .css('resources/css/app.css', 'public/css/app.css')
+  .react();
