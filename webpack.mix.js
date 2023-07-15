fafaf9f43f5bcd9c6059a990d377d97595875b9e
RodSoriano <rsoriano.jose@gmail.com>
2023-07-15 16:10:22 -0600

    Update webpack.mix.js, set public path.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'js').css('resources/css/app.css', 'css');
+mix.js('resources/js/app.js', 'public/js').css('resources/css/app.css', 'public/css');
