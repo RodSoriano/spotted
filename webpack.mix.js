e47fd13a9b8ffc89730c6aec76a014096cceedd1
RodSoriano <rsoriano.jose@gmail.com>
2023-07-15 16:05:19 -0600

    Update js, css and webpack.mix.js file to default.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,3 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .setPublicPath('public');
+mix.js('resources/js/app.js', 'js').css('resources/css/app.css', 'css');
