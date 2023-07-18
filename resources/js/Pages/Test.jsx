c2523bd3f16292bf7ac53b201757ec124ac6d804
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 15:22:11 -0600

    Update test controller to use Index.jsx file

--- resources/js/Pages/Test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Head } from '@inertiajs/inertia-react';
-
-export default function Test() {
-  return (
-    <h1>Welcome</h1>
-  );
-}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Web\TestController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', [TestController::class, 'show']);
-
-Route::get('test-index', function () {
+Route::get('test', function () {
     return Inertia::render('Index');
 });
