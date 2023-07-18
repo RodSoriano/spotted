2e7e0365d440ef525958554aae7c238f4435f4b3
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 13:59:48 -0600

    Add storage path into tailwind config file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./storage/framework/views/*.php",
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.jsx",
+    "./resources/**/*.vue",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
