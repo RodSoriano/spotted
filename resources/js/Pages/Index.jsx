8eee3f4cd36c7a4f3cfba25d8673ae09ff5472a2
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 12:38:07 -0600

    Create basic index component.

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Head } from '@inertiajs/inertia-react';
+
+export default function Index() {
+  return (
+    <p>
+      This is a message from the index.
+      Enjoy!
+    </p>
+  );
+}
