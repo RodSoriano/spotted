a253552d1ec8b62cf1415184a46bb491a5919713
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 14:00:09 -0600

    Update webpack.mix.js to use tailwind.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js/app.js')
   .postCss('resources/css/app.css', 'public/css/app.css', [
-    require('./tailwind.config'),
+    require('tailwindcss'),
   ])
   .react();
