1ec9d5ca5d6934ad7fda2e043555a083287656b6
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 23:49:10 -0600

    Add get, post endpoints for registerController.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Web\RegisterController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Index');
 });
+
+Route::get('register', [RegisterController::class, 'show']);
+Route::post('register', [RegisterController::class, 'store']);
