a8a88b701101b3db8a16ec5a4b39d4c8f177b554
RodSoriano <rsoriano.jose@gmail.com>
2023-07-19 02:15:31 -0600

    Create check-in route, controller and view.

---
+++ resources/js/Pages/User/CheckIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../Layout";
+
+export default function CheckIn() {
+  return (
+    <>
+      Welcome to the check-in!
+    </>
+  );
+}
+
+CheckIn.layout = page => <Layout children={page} />
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\CheckInController;
 use App\Http\Controllers\Web\RegisterController;
 use App\Http\Controllers\Web\ReservationController;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,5 @@ Route::get('register', [RegisterController::class, 'show']);
 Route::post('register', [RegisterController::class, 'store']);
 
 Route::get('reservations', [ReservationController::class, 'index']);
+
+Route::get('check-in', CheckInController::class, 'show');
