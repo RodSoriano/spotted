fa401972104b83b51e6f410f5e4eeead9f1ad830
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 21:10:55 -0600

    Create basic index component.

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { Head } from '@inertiajs/inertia-react';
+import { Head, Link } from '@inertiajs/inertia-react';
 
 export default function Index() {
   return (
-    <p>
-      This is a message from the index.
-      Enjoy!
-    </p>
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+      <Head>
+        <title>Booking</title>
+      </Head>
+
+      <Link
+        href="/"
+        className="bg-blue-500 text-white px-8 py-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105"
+      >
+        Register
+      </Link>
+      <Link
+        href="/"
+        className="bg-purple-500 text-white px-8 py-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105"
+      >
+        Book your space
+      </Link>
+      <Link
+        href="/"
+        className="bg-green-500 text-white px-8 py-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105"
+      >
+        Check-in
+      </Link>
+    </div>
   );
 }
