860860c0d0fc4b8055c973bb2c1109efb915904e
RodSoriano <rsoriano.jose@gmail.com>
2023-07-19 02:15:50 -0600

    Update Index.jsx to use the proper routing.

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -12,13 +12,13 @@ export default function Index() {
         Register
       </Link>
       <Link
-        href="/"
+        href="/reservations"
         className="bg-purple-500 text-white px-8 py-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105"
       >
         Book a space
       </Link>
       <Link
-        href="/"
+        href="/check-in"
         className="bg-green-500 text-white px-8 py-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105"
       >
         Check-in
