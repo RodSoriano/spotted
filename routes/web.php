36fe30b8c1f23f999214e5a86e8c07781f087032
RodSoriano <rsoriano.jose@gmail.com>
2023-07-18 21:13:51 -0600

    Update web routing for home page.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('test', function () {
     return Inertia::render('Index');
 });
