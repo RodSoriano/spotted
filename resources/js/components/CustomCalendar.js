06899452685da499e2a9bfc28bc9238a46a600de
RodSoriano <rsoriano.jose@gmail.com>
2023-07-20 14:15:48 -0600

    Update custom calendar component.

--- resources/js/components/CustomCalendar.js
+++ resources/js/components/CustomCalendar.js
@@ -5,23 +5,21 @@ import Label from './Label';
 
 const CustomCalendar = ({ inputLabel, selectedDate, onDateChange }) => {
   return (
-    <>
-      <div className="flex flex-col items-start">
-        <Label labelName={inputLabel} />
-        <div className="flex justify-center mb-4">
-          <DatePicker
-            selected={selectedDate}
-            onChange={onDateChange}
-            showYearDropdown
-            showMonthDropdown
-            dropdownMode="select"
-            popperClassName="z-50"
-            className="border rounded-lg p-2 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            calendarClassName="bg-white shadow-lg rounded-lg p-4"
-          />
-        </div>
+    <div className="flex flex-col items-start mb-4">
+      <Label labelName={inputLabel} />
+      <div className="flex justify-center">
+        <DatePicker
+          selected={selectedDate}
+          onChange={onDateChange}
+          showYearDropdown
+          showMonthDropdown
+          dropdownMode="select"
+          popperClassName="z-50"
+          className="border rounded-lg p-2 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          calendarClassName="bg-white shadow-lg rounded-lg p-4"
+        />
       </div>
-    </>
+    </div>
   );
 };
 
