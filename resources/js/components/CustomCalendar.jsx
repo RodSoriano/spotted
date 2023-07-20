3809fa63d78d05627b388f4e65881fa817fa5e71
RodSoriano <rsoriano.jose@gmail.com>
2023-07-20 13:15:06 -0600

    Update date component so it can be re used elsewhere.

--- resources/js/components/CustomCalendar.jsx
+++ resources/js/components/CustomCalendar.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-export default function CustomCalendar({ ...args }) {
-  const [selectedDate, setSelectedDate] = args;
-
+export default function CustomCalendar({ inputLabel, selectedDate, onDateChange }) {
   return (
-    <div className="card flex justify-center items-center bg-white rounded-lg shadow-lg p-4">
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-        showYearDropdown
-        showMonthDropdown
-        dropdownMode="select"
-        popperClassName="z-50"
-        className="border rounded-lg p-2 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        calendarClassName="bg-white shadow-lg rounded-lg p-4"
-      />
+    <div className="flex flex-col items-start">
+      <label htmlFor={inputLabel} className="text-gray-800 font-medium mb-2">
+        {inputLabel}
+      </label>
+      <div className="flex justify-center">
+        <DatePicker
+          selected={selectedDate}
+          onChange={onDateChange}
+          showYearDropdown
+          showMonthDropdown
+          dropdownMode="select"
+          popperClassName="z-50"
+          className="border rounded-lg p-2 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          calendarClassName="bg-white shadow-lg rounded-lg p-4"
+        />
+      </div>
     </div>
   );
 }
