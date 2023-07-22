3ef32288757977d784177e28dd65c63fe7992615
RodSoriano <rsoriano.jose@gmail.com>
2023-07-21 19:02:56 -0600

    Create validation for storing new user.

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string'],
+            'last_name' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'date_of_birth' => Rule::prohibitedIf($this->invalidDate()),
+            'emergency_contact_name' => ['required', 'string'],
+            'emergency_contact_number' => ['required', 'string'],
+            'photo' => [],
+        ];
+    }
+
+    private function invalidDate(): bool
+    {
+        $inputDate = strtotime($this->date_of_birth);
+        $currentYear = date('y');
+        $isValid = true;
+
+        [$inputMonth, $inputYear] = explode('/', date('m/y', $inputDate), 2);
+
+        if ($inputMonth > 12) {
+            $isValid = false;
+        }
+
+        if ($inputYear > ((int) $currentYear - 5)) {
+            $isValid = false;
+        }
+
+        return !$isValid;
+    }
+}
