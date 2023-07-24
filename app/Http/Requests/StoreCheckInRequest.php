95a5d6539b65cb88ae51633b12d3069fe294f6dd
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 00:14:58 -0600

    Create store check in form request for validation.

---
+++ app/Http/Requests/StoreCheckInRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreCheckInRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'date' => Rule::prohibitedIf($this->invalidDate()),
+        ];
+    }
+
+    private function invalidDate(): bool
+    {
+        $inputDate = strtotime($this->date);
+        $isValid = true;
+
+        [$currentMonth, $currentDay, $currentYear] = explode('/', date('m/d/y'), 2);;
+        [$inputMonth, $inputDay, $inputYear] = explode('/', date('m/d/y', $inputDate), 2);
+
+        if ($inputDay < $currentDay) {
+            $isValid = false;
+        }
+
+        if ($inputMonth !== $currentMonth) {
+            $isValid = false;
+        }
+
+        if ($inputYear !== $currentYear) {
+            $isValid = false;
+        }
+
+        return !$isValid;
+    }
+}
