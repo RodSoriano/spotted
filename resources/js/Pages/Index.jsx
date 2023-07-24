7b0f73415b1001076de7be803fd2651317f73a3f
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 00:14:34 -0600

    Update index reservation link.

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -12,7 +12,7 @@ const Index = () => {
         Register
       </Link>
       <Link
-        href="/reservations"
+        href="/reservation"
         className="bg-purple-500 text-white px-8 py-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105"
       >
         Book a space
