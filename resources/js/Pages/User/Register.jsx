d8e5372ebb114879b7206ccb95a31d8b198d22a7
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 00:14:20 -0600

    Update register react component.

--- resources/js/Pages/User/Register.jsx
+++ resources/js/Pages/User/Register.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
-import { Inertia } from '@inertiajs/inertia';
 import Layout from '../Layout';
+import { Inertia } from '@inertiajs/inertia';
+
+import Title from '../../components/Title';
 import Label from '../../components/Label';
 import FormInput from '../../components/FormInput';
-import { charactersOnly } from '../../utils/formatters';
-import PhotoUploader from '../../components/PhotoUploader';
 import CustomCalendar from '../../components/CustomCalendar';
+import PhotoUploader from '../../components/PhotoUploader';
+import Button from '../../components/Button';
+import Alert from '../../components/Alert';
+
+import { charactersOnly } from '../../utils/formatters';
 
 const Register = () => {
+  const [errors, setErrors] = useState([]);
+  const [status, setStatus] = useState(false);
+
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState(null);
   const [emergencyName, setEmergencyName] = useState('');
   const [emergencyPhone, setEmergencyPhone] = useState('');
-  // const [photo, setPhoto] = useState(null);
+
+  // needs refactoring, photo upload.
+  const [photo, setPhoto] = useState('...');
 
   const handleFirstName = (e) => {
     const inputValue = charactersOnly(e);
@@ -43,12 +53,13 @@ const Register = () => {
     setEmergencyPhone(e.target.value);
   };
 
-  // const handlePhotoUpload = (photoData) => {
-  //   setPhoto(photoData);
-  // };
+  const handlePhotoUpload = (photoData) => {
+    setPhoto(photoData);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const values = {
       first_name: firstName,
       last_name: lastName,
@@ -56,62 +67,67 @@ const Register = () => {
       date_of_birth: dateOfBirth,
       emergency_contact_name: emergencyName,
       emergency_contact_number: emergencyPhone,
-      // photo: photo,
+      photo: photo,
     };
-    Inertia.post('/register', values);
+
+    Inertia.post('/register', values, {
+      onSuccess() {
+        //
+      },
+      onError(errors) {
+        setStatus(422);
+        setErrors(Object.entries(errors));
+      }
+    });
   };
 
   return (
     <>
-      <h1 className="text-xl font-bold mb-4 text-gray-800 flex items-center justify-center">
-        Welcome to the Park
-      </h1>
-
-      <p className="mb-4 text-gray-800 flex items-center justify-center">
-        Please register so you can book a space
-      </p>
+      <Title
+        h1={'Welcome to the Park'}
+        paragraph={'Please register so you can book a space.'}
+      />
 
       <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
         <FormInput
-          inputName="First Name"
+          inputLabel='First Name'
           inputValue={firstName}
           onChangeEvent={handleFirstName}
         />
         <FormInput
-          inputName="Last Name"
+          inputLabel='Last Name'
           inputValue={lastName}
           onChangeEvent={handleLastName}
         />
         <FormInput
-          inputName="Email"
+          inputLabel='Email'
           inputValue={email}
           onChangeEvent={handleEmail}
         />
         <CustomCalendar
-          inputLabel="Date of Birth"
+          inputLabel='Date of Birth'
           selectedDate={dateOfBirth}
           onDateChange={handleDateChange}
         />
         <FormInput
-          inputName="Emergency Contact Name"
+          inputLabel='Emergency Contact Name'
           inputValue={emergencyName}
           onChangeEvent={handleEmergencyName}
         />
         <FormInput
-          inputName="Emergency Contact Phone"
+          inputLabel='Emergency Contact Phone'
           inputValue={emergencyPhone}
           onChangeEvent={handleEmergencyPhone}
         />
 
-        {/* <Label labelName="Upload Your Picture" />
-        <PhotoUploader onPhotoUpload={handlePhotoUpload} /> */}
+        <Label labelName='Upload Your Picture' />
+        <PhotoUploader onPhotoUpload={handlePhotoUpload} />
 
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300"
-        >
-          Join
-        </button>
+        {status && (<Alert message={errors} />)}
+
+        <div className='flex items-center justify-center'>
+          <Button type={'submit'} message={'Join'} />
+        </div>
       </form>
     </>
   );
