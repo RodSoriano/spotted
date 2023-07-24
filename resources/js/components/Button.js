0b4ace8f46bbb1037c596bf513cdbd0f4c4224d3
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 00:16:50 -0600

    Update button style to be a round button.

--- resources/js/components/Button.js
+++ resources/js/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ type, message }) => {
   const styles = {
-    button: 'bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300'
+    button: 'bg-blue-500 text-white py-2 px-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105'
   };
 
   return (
