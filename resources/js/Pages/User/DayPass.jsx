7c25531815701a2244d0f30ebbfd4fbc1871c50d
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 22:19:23 -0600

    Update daypass component with proper structure.

---
+++ resources/js/Pages/User/DayPass.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Layout from '../Layout';
+import { Inertia } from '@inertiajs/inertia';
+
+import Title from '../../components/Title';
+
+const DayPass = ({ user, date }) => {
+
+  console.log(user)
+  console.log(date)
+
+  return (
+    <>
+      <Title h1="Great to See You Again!" />
+
+      <div className="flex items-center justify-center mb-6">
+        <div className="w-24 h-24 bg-gray-300 rounded-full">
+          {/* photo */}
+        </div>
+      </div>
+      <div className="text-center">
+        <h2 className="text-xl font-semibold mb-2">
+          {user.first_name} {user.last_name}
+        </h2>
+        <p className="text-gray-600">Total: ${user.fee}</p>
+        <p className="text-gray-600 mt-2">Date: {date}</p>
+      </div>
+    </>
+  );
+};
+
+export default DayPass;
+
+DayPass.layout = page => <Layout children={page} />;
