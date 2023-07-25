8fdbb4ca862d68b5af3f51eb2217e6cbd53ee2d2
RodSoriano <rsoriano.jose@gmail.com>
2023-07-25 01:08:31 -0600

    Update button component to have a default color.

--- resources/js/components/Button.js
+++ resources/js/components/Button.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const Button = ({ type, message }) => {
+const Button = ({ type, message, color }) => {
   const styles = {
-    button: 'bg-blue-500 text-white py-2 px-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105'
+    button: `${color} text-white py-2 px-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105`
   };
 
   return (
-    <button type={type} className={styles.button}>
+    <button className={styles.button} type={type}>
       {message}
     </button>
   );
+};
+
+Button.defaultProps = {
+  color: 'bg-blue-500',
 };
 
 export default Button;
