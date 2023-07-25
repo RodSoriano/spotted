09d5c89259f36f3f06b777ec8ea8861b8ea5fc9f
RodSoriano <rsoriano.jose@gmail.com>
2023-07-25 01:28:31 -0600

    Update user service to return a message to index.

--- app/Services/User/UserCreator.php
+++ app/Services/User/UserCreator.php
@@ -5,24 +5,23 @@ namespace App\Services\User;
 use App\Enum\UserRole;
 use App\Enum\UserStatus;
 use App\Models\User;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Routing\Redirector;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class UserCreator
 {
-    // needs refactoring, photo attribute
-    public function register(array $user): Redirector|RedirectResponse
+    public function register(array $user): Response
     {
-        $user['role_id'] = UserRole::USER->value;
-        $user['status'] = UserStatus::ACCEPTED->value;
         $user['fee'] = 10.00;
-
+        $user['status'] = UserStatus::ACCEPTED->value;
+        $user['role_id'] = UserRole::USER->value;
         $user['date_of_birth'] = $this->formatDate($user['date_of_birth']);
 
         User::create($user);
 
-        // needs to redirect and user needs to see created! on index screen.
-        return redirect('/')->with('Mensaje', 'Tu usuario fue creado');
+        return Inertia::render('Index', [
+            'message' => 'Your user has been created!',
+        ]);
     }
 
     private function formatDate(string $date): string
