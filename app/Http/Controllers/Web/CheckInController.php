cc15665d7a731437c32bfdb0ea1a79babbfd6c6e
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 22:18:57 -0600

    Update show arguments to pass date object.

--- app/Http/Controllers/Web/CheckInController.php
+++ app/Http/Controllers/Web/CheckInController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CheckReservationRequest;
 use App\Http\Requests\StoreCheckInRequest;
+use App\Models\CheckIn;
 use App\Models\User;
 use App\Services\CheckIn\ReservationCreator;
 use \Illuminate\Http\RedirectResponse;
@@ -27,9 +28,11 @@ class CheckInController extends Controller
     public function show(Request $request): Response
     {
         $user = User::where('email', $request->user['email'])->first();
+        $date = CheckIn::where('user_id', $user->id)->latest()->pluck('date')->first();
 
         return Inertia::render('User/DayPass', [
             'user' => $user,
+            'date' => $date,
         ]);
     }
 
