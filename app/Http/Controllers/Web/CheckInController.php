aedc0212b0a2ca6358ba69e349c969bc2e42d340
RodSoriano <rsoriano.jose@gmail.com>
2023-07-24 18:01:52 -0600

    Update check-in controller.

--- app/Http/Controllers/Web/CheckInController.php
+++ app/Http/Controllers/Web/CheckInController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCheckInRequest;
+use App\Models\User;
+use App\Services\CheckIn\ReservationCreator;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class CheckInController extends Controller
 {
-    public function show(): Response
+    /* check-in */
+    public function index(): Response
     {
         return Inertia::render('User/CheckIn');
+    }
+
+    /* reservation */
+    public function booking(): Response
+    {
+        return Inertia::render('User/Reservation');
+    }
+
+    // needs refactoring, proper redirect response.
+    public function store(StoreCheckInRequest $request, ReservationCreator $checkInService): mixed
+    {
+        $booking = $request->validated();
+        $response = $checkInService->createReservation($booking);
+
+        return $response;
     }
 }
