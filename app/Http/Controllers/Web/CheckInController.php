9eaae8fb79832553320c232e7dc6d7990ec8d4fb
RodSoriano <rsoriano.jose@gmail.com>
2023-07-26 00:41:15 -0600

    Order the methods so the file is more readable.

--- app/Http/Controllers/Web/CheckInController.php
+++ app/Http/Controllers/Web/CheckInController.php
@@ -8,32 +8,17 @@ use App\Http\Requests\StoreCheckInRequest;
 use App\Models\CheckIn;
 use App\Models\User;
 use App\Services\CheckIn\ReservationCreator;
-use \Illuminate\Http\RedirectResponse;
+use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class CheckInController extends Controller
 {
-    public function index(): Response
-    {
-        return Inertia::render('User/CheckIn');
-    }
-
     public function bookingForm(): Response
     {
         return Inertia::render('User/Reservation');
-    }
-
-    public function show(Request $request): Response
-    {
-        $user = User::where('email', $request->user['email'])->first();
-        $date = CheckIn::where('user_id', $user->id)->latest()->pluck('date')->first();
-
-        return Inertia::render('User/DayPass', [
-            'user' => $user,
-            'date' => $date,
-        ]);
     }
 
     public function store(StoreCheckInRequest $request, ReservationCreator $checkInService): Response
@@ -44,6 +29,11 @@ class CheckInController extends Controller
         return $response;
     }
 
+    public function index(): Response
+    {
+        return Inertia::render('User/CheckIn');
+    }
+
     public function bookingCheck(CheckReservationRequest $request): RedirectResponse
     {
         $data = $request->validated();
@@ -52,5 +42,20 @@ class CheckInController extends Controller
             [CheckInController::class, 'show'],
             ['user' => $data]
         );
+    }
+
+    public function show(Request $request): Response
+    {
+        try {
+            $user = User::where('email', $request->user['email'])->first();
+            $date = CheckIn::where('user_id', $user->id)->latest()->pluck('date')->first();
+
+            return Inertia::render('User/DayPass', [
+                'user' => $user,
+                'date' => $date,
+            ]);
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
     }
 }
