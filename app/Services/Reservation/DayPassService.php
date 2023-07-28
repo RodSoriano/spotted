7a44e7274f11f33fe4a396605dcdf6ce7f33ca90
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 14:36:28 -0600

    Create dayPass service to get users data.

---
+++ app/Services/Reservation/DayPassService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services\Reservation;
+
+use App\Models\Reservation;
+use App\Models\User;
+use App\Services\ServiceHelper;
+use Illuminate\Support\Facades\Storage;
+
+class DayPassService
+{
+    use ServiceHelper;
+
+    public function getData(string $email): array
+    {
+        $user = User::where('email', $email)->first();
+        $date = Reservation::where('user_id', $user->id)->latest()->pluck('date')->first();
+
+        $photoUrl = Storage::temporaryUrl(
+            $user->photo,
+            now()->addMinutes(5)
+        );
+
+        $user->photo = $photoUrl;
+
+        return [
+            'user' => $user,
+            'date' => $date,
+        ];
+    }
+}
