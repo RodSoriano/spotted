1ffb4aaced60cc57a18b5334824fede09c93c975
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 15:53:05 -0600

    Update table component with jsx extension.

--- resources/js/components/Title.jsx
+++ resources/js/components/Title.jsx
@@ -3,18 +3,13 @@ import React from 'react';
 const Title = ({ h1, paragraph }) => {
   const styles = {
     h1: 'text-xl font-bold mb-4 text-gray-800 flex items-center justify-center',
-    paragraph: 'mb-4 text-gray-800 flex items-center justify-center'
+    paragraph: 'mb-4 text-gray-800 flex items-center justify-center',
   };
 
   return (
     <div>
-      <h1 className={styles.h1}>
-        {h1}
-      </h1>
-
-      <p className={styles.paragraph}>
-        {paragraph && paragraph}
-      </p>
+      <h1 className={styles.h1}>{h1}</h1>
+      {paragraph && <p className={styles.paragraph}>{paragraph}</p>}
     </div>
   );
 };
