bcb02e2903567f2f0b28ff8461ca015ca141b28a
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 15:51:12 -0600

    Update Label component with jsx extension.

--- resources/js/components/Label.jsx
+++ resources/js/components/Label.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Label = ({ labelName }) => {
+  const styles = {
+    label: 'text-gray-800 font-medium',
+  };
+
   return (
-    <label
-      htmlFor={labelName}
-      className="text-gray-800 font-medium"
-    >
+    <label htmlFor={labelName} className={styles.label}>
       {labelName}
     </label>
   );
