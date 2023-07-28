cf26404c1ac59bd9ab53e20f105db3e72c97f283
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 00:38:08 -0600

    Update register component to use terms component.

--- resources/js/Pages/User/Register.jsx
+++ resources/js/Pages/User/Register.jsx
@@ -6,14 +6,15 @@ import Title from '../../components/Title';
 import Label from '../../components/Label';
 import FormInput from '../../components/FormInput';
 import CustomCalendar from '../../components/CustomCalendar';
+import TermsAndConditions from '../Popups/TermsAndConditions';
 import Button from '../../components/Button';
 import Alert from '../../components/Alert';
 
 import { charactersOnly } from '../../utils/formatters';
 
 const Register = () => {
-  const [errors, setErrors] = useState([]);
   const [status, setStatus] = useState(false);
+  const [errors, setErrors] = useState([]);
 
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -22,7 +23,7 @@ const Register = () => {
   const [emergencyName, setEmergencyName] = useState('');
   const [emergencyPhone, setEmergencyPhone] = useState('');
   const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [isAccepted, setIsAccepted] = useState(false);
+  const [accept, setAccept] = useState(false);
 
   const handleFirstName = (e) => {
     const inputValue = charactersOnly(e);
@@ -38,8 +39,8 @@ const Register = () => {
     setEmail(e.target.value);
   };
 
-  const handleDateChange = (selectedDate) => {
-    setDateOfBirth(selectedDate);
+  const handleDateChange = (e) => {
+    setDateOfBirth(e);
   };
 
   const handleEmergencyName = (e) => {
@@ -51,30 +52,21 @@ const Register = () => {
     setEmergencyPhone(e.target.value);
   };
 
-  // Needs refactoring, photo upload.
   const handlePhotoSelect = (e) => {
     const photoInput = e.target;
     const photo = photoInput.files[0];
 
     if (photo) {
       setSelectedPhoto(photo);
-
-      // We can also read the photo as a data URL or perform other operations
-      // const reader = new FileReader();
-      // reader.onload = function (e) {
-      //   const dataURL = e.target.result;
-      //   console.log('Data URL:', dataURL);
-      // };
-      // reader.readAsDataURL(photo);
     }
   };
 
-  const handleTermsAndConditions = () => {
-    if (confirm('Terms and Conditions.')) {
-      setIsAccepted(true);
-    } else {
-      setIsAccepted(false);
-    }
+  const handleAccept = () => {
+    setAccept(true);
+  };
+
+  const handleDecline = () => {
+    setAccept(false);
   };
 
   const handleSubmit = (e) => {
@@ -142,32 +134,17 @@ const Register = () => {
         />
 
         <Label labelName='Upload Your Picture' />
-        <div className='mt-4 mb-4'>
+        <div className='mt-4 mb-4 flex items-center justify-center'>
           <input type='file' name='photo' accept='image/*' onChange={handlePhotoSelect} />
-          {/*selectedPhoto && (
-            <div>
-              We can add more info over here from the photo
-              <h3>Selected Photo Information:</h3>
-              <p>Name: {selectedPhoto.name}</p>
-              <p>Size: {selectedPhoto.size} bytes</p>
-              <p>Type: {selectedPhoto.type}</p>
-            </div>
-          ) */}
         </div>
 
-        {!isAccepted &&
-          <a
-            className='bg-purple-500 text-white py-2 px-4 rounded-full text-lg mb-4 transform transition-transform hover:scale-105'
-            href="#"
-            onClick={handleTermsAndConditions}
-          >
-            Terms and Conditions
-          </a>
-        }
+        <div className='flex items-center justify-center'>
+          <TermsAndConditions onAccept={handleAccept} onDecline={handleDecline} />
+        </div>
 
         {status && (<Alert message={errors} />)}
 
-        {isAccepted &&
+        {accept &&
           <div className='flex items-center justify-center'>
             <Button type={'submit'} message={'Join'} />
           </div>
