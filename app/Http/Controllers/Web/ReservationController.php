38b8b07b242f56225ce951d04ceb482d6e7f56fb
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 02:10:11 -0600

    Update checkin to reservation in controller.

--- app/Http/Controllers/Web/ReservationController.php
+++ app/Http/Controllers/Web/ReservationController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CheckReservationRequest;
-use App\Http\Requests\StoreCheckInRequest;
-use App\Models\CheckIn;
+use App\Http\Requests\StoreReservationRequest;
+use App\Models\Reservation;
 use App\Models\User;
-use App\Services\CheckIn\ReservationCreator;
+use App\Services\Reservation\ReservationCreator;
 use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -15,17 +15,17 @@ use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class CheckInController extends Controller
+class ReservationController extends Controller
 {
     public function bookingForm(): Response
     {
         return Inertia::render('User/Reservation');
     }
 
-    public function store(StoreCheckInRequest $request, ReservationCreator $checkInService): Response
+    public function store(StoreReservationRequest $request, ReservationCreator $reservationService): Response
     {
         $booking = $request->validated();
-        $response = $checkInService->createReservation($booking);
+        $response = $reservationService->createReservation($booking);
 
         return $response;
     }
@@ -40,7 +40,7 @@ class CheckInController extends Controller
         $data = $request->validated();
 
         return redirect()->action(
-            [CheckInController::class, 'show'],
+            [ReservationController::class, 'show'],
             ['user' => $data]
         );
     }
@@ -49,7 +49,7 @@ class CheckInController extends Controller
     {
         try {
             $user = User::where('email', $request->user['email'])->first();
-            $date = CheckIn::where('user_id', $user->id)->latest()->pluck('date')->first();
+            $date = Reservation::where('user_id', $user->id)->latest()->pluck('date')->first();
 
             $photoUrl = Storage::temporaryUrl(
                 $user->photo,
