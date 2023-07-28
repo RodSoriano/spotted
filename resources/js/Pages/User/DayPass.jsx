ec40ece7f599a781aab013433796d7c9fe4f9d4d
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 17:00:30 -0600

    Refactor dayPass component.

--- resources/js/Pages/User/DayPass.jsx
+++ resources/js/Pages/User/DayPass.jsx
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from '../Layout';
-
 import Title from '../../components/Title';
 
 const DayPass = ({ user, date }) => {
+  const styles = {
+    container: 'flex items-center justify-center mb-6 w-24 h-24 bg-gray-300 rounded-full',
+    userProfile: 'w-full h-full rounded-full object-cover',
+    userInfo: 'text-center',
+    userName: 'text-xl font-semibold mb-2',
+    userTotal: 'text-gray-600',
+    userDate: 'text-gray-600 mt-2',
+  };
 
   return (
     <>
-      <Title h1="Great to See You Again!" />
+      <Title h1='Great to See You Again!' />
 
-      <div className="flex items-center justify-center mb-6 w-24 h-24 bg-gray-300 rounded-full">
+      <div className={styles.container}>
         <img
           src={user.photo}
-          alt="User profile picture."
-          className="w-full h-full rounded-full object-cover"
+          alt='User profile'
+          className={styles.userProfile}
         />
       </div>
-      <div className="text-center">
-        <h2 className="text-xl font-semibold mb-2">
+      <div className={styles.userInfo}>
+        <h2 className={styles.userName}>
           {user.first_name} {user.last_name}
         </h2>
-        <p className="text-gray-600">Total: ${user.fee}</p>
-        <p className="text-gray-600 mt-2">Date: {date}</p>
+        <p className={styles.userTotal}>Total: <strong>${user.fee}</strong></p>
+        <p className={styles.userDate}>Date: <strong>{date}</strong></p>
       </div>
     </>
   );
