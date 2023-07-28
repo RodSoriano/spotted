1148b97dc27bf43a0b3add9a367a4dfb436f1807
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 05:18:30 -0600

    Update reservation service with proper request handlers.

--- app/Services/Reservation/ReservationCreator.php
+++ app/Services/Reservation/ReservationCreator.php
@@ -12,28 +12,36 @@ class ReservationCreator
 {
     use ServiceHelper;
 
-    // needs refactoring, also check if the reservation limit has been reached for an specified date.
-    public function createReservation(array $reservation): Response
+    public function createOrFail(array $reservation): Response
     {
         $email = $reservation['email'];
         $reservationExists = $this->reservationExists($email);
 
         if ($reservationExists) {
-            return Inertia::render('User/DayPass', [
-                'user' => $reservationExists['user'],
-                'date' => $reservationExists['date'],
-            ]);
-        } else {
-            $reservation['user_id'] = $this->getUserIdByEmail($email);;
-            $reservation['is_done'] = false;
-            $reservation['date'] = $this->formatDate($reservation['date']);
-
-            Reservation::create($reservation);
-
-            return Inertia::render('Index', [
-                'message' => 'Your reservation has been created.',
-            ]);
+            return $this->redirect($reservationExists);
         }
+
+        $date = $this->formatDate($reservation['date']);;
+        $bookingLimitReached = $this->reservationLimitCheck($date);
+
+        if ($bookingLimitReached) {
+            return $this->redirect();
+        }
+
+        return $this->create($reservation);
+    }
+
+    private function create(array $reservation): Response
+    {
+        $reservation['user_id'] = $this->getUserIdByEmail($reservation['email']);;
+        $reservation['is_done'] = false;
+        $reservation['date'] = $this->formatDate($reservation['date']);
+
+        Reservation::create($reservation);
+
+        return Inertia::render('Index', [
+            'message' => 'Your reservation has been created.',
+        ]);
     }
 
     private function reservationExists(string $email): array
@@ -48,9 +56,39 @@ class ReservationCreator
 
         if ($reservation === 0) {
             $result['user'] = User::where('id', $userId)->first();
-            $result['date'] = Reservation::where('user_id', $userId)->latest()->pluck('date')->first();
+
+            $result['date'] = Reservation::where('user_id', $userId)
+                ->latest()
+                ->pluck('date')
+                ->first();
         }
 
         return $result;
+    }
+
+    private function reservationLimitCheck(string $date): bool|int
+    {
+        $collection = Reservation::where('date', $date)->get();
+        $count = count($collection);
+
+        if ($count < 5) {
+            return false;
+        }
+
+        return $count;
+    }
+
+    private function redirect(?array $data = []): Response
+    {
+        if ($data) {
+            return Inertia::render('User/DayPass', [
+                'user' => $data['user'],
+                'date' => $data['date'],
+            ]);
+        } else {
+            return Inertia::render('Index', [
+                'message' => 'Sorry, no spots are left, try another date.',
+            ]);
+        }
     }
 }
