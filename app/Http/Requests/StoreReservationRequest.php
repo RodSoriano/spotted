ecd5a87a7cc486a91053e31ded7dd5c021d964ef
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 05:08:35 -0600

    Update message for email in form request.

--- app/Http/Requests/StoreReservationRequest.php
+++ app/Http/Requests/StoreReservationRequest.php
@@ -17,7 +17,7 @@ class StoreReservationRequest extends FormRequest
     {
         return [
             'email' => 'This email is not associated with a registered account.',
-            'date' => 'The date must be within this month and in the future',
+            'date' => 'The date must be in the future and within the month',
         ];
     }
 
