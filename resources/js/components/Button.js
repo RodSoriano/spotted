23422331706e657defe2952765770dddc71c6f7f
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 00:34:22 -0600

    Update button component with on click argument.

--- resources/js/components/Button.js
+++ resources/js/components/Button.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const Button = ({ type, message, color }) => {
-  const styles = {
-    button: `${color} text-white py-2 px-4 rounded-full font-bold text-lg mb-4 transform transition-transform hover:scale-105`
-  };
+const Button = ({ type, message, color, onClick }) => {
+  const buttonClass = `${color} text-white py-2 px-4 rounded-full font-bold text-lg mb-4 mx-4 transform transition-transform hover:scale-105`;
 
   return (
-    <button className={styles.button} type={type}>
+    <button className={buttonClass} type={type} onClick={onClick}>
       {message}
     </button>
   );
 };
 
 Button.defaultProps = {
+  type: 'button',
+  message: '',
   color: 'bg-blue-500',
+  onClick: () => { },
 };
 
 export default Button;
