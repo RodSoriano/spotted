81f7147f0eded87eb64d0c0f2021245d3d44a7a5
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 15:44:49 -0600

    Update formInput component with jsx extension.

--- resources/js/components/FormInput.jsx
+++ resources/js/components/FormInput.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Label from './Label';
 
 const FormInput = ({ inputLabel, inputValue, onChangeEvent }) => {
+  const styles = {
+    container: 'mb-4',
+    input: 'mt-1 px-4 py-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500',
+  };
+
   return (
-    <div className="mb-4">
+    <div className={styles.container}>
       <Label labelName={inputLabel} />
       <input
-        className="mt-1 px-4 py-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-        type="text"
+        className={styles.input}
+        type='text'
         name={inputLabel}
         value={inputValue}
         onChange={onChangeEvent}
