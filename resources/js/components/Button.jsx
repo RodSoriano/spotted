153a5d2fd3499feabc396ec436f8cabcc8581c9d
RodSoriano <rsoriano.jose@gmail.com>
2023-07-28 23:20:57 -0600

    Update with forwardRef method to use button inside popup.

--- resources/js/components/Button.jsx
+++ resources/js/components/Button.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Button = ({ type, message, color, onClick }) => {
-  const buttonClass = `${color} text-white py-2 px-4 rounded-full font-bold text-lg mb-4 mx-4 transform transition-transform hover:scale-105`;
+const Button = React.forwardRef(({ type, message, color, onClick }, innerRef) => {
+  const buttonClass = `${color} text-white py-2 px-4 rounded-full font-bold text-lg mb-4 mx-2 transform transition-transform hover:scale-105`;
 
   return (
     <button className={buttonClass} type={type} onClick={onClick}>
       {message}
     </button>
   );
-};
+});
 
 Button.defaultProps = {
   type: 'button',
