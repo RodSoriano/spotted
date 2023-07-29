09ee66e088f414365eaeb21c8e0387b7dbadc0db
RodSoriano <rsoriano.jose@gmail.com>
2023-07-29 00:26:53 -0600

    Update formInput so the input is smaller.

--- resources/js/components/FormInput.jsx
+++ resources/js/components/FormInput.jsx
@@ -4,12 +4,12 @@ import Label from './Label';
 const FormInput = ({ inputLabel, inputValue, onChangeEvent }) => {
   const styles = {
     container: 'mb-4',
-    input: 'mt-1 px-4 py-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500',
+    input: 'mt-1 px-4 py-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500 w-2/3',
   };
 
   return (
     <div className={styles.container}>
-      <Label labelName={inputLabel} />
+      <Label labelName={inputLabel} /><br />
       <input
         className={styles.input}
         type='text'
