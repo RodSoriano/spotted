b756a08e72b7e6912599592d9ea95bd71b5e595a
RodSoriano <rsoriano.jose@gmail.com>
2023-07-31 02:18:06 -0600

    Update index page to use translated texts.

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -3,24 +3,33 @@ import Layout from './Layout';
 
 import CustomLink from '../components/CustomLink';
 import Alert from '../components/Alert';
+import { Link } from '@inertiajs/inertia-react';
 
-const Index = ({ message }) => {
+const Index = ({ localeText, message }) => {
 
   return (
     <>
       {message && <Alert message={message} />}
 
-      <CustomLink hrefProp={'/register'} message={'Register'} />
+      <CustomLink hrefProp={'/register'} message={localeText.buttons.register} />
 
-      <CustomLink color={'bg-purple-500'} hrefProp={'/reservation'} message={'Book a Space'} />
+      <CustomLink color={'bg-purple-500'} hrefProp={'/reservation'} message={localeText.buttons.book} />
 
-      <CustomLink color={'bg-green-500'} hrefProp={'/check-in'} message={'Check In'} />
+      <CustomLink color={'bg-green-500'} hrefProp={'/check-in'} message={localeText.buttons.checkIn} />
+
+      <div className='flex items-center'>
+        <p>
+          {localeText.localeSwitch}
+          <Link className='text-blue-500 underline' href='/language'> {localeText.clickHere} </Link>
+        </p>
+      </div>
     </>
   );
 };
 
 Index.defaultProps = {
-  message: false
+  localeText: '',
+  message: false,
 };
 
 export default Index;
