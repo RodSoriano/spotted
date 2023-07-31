413be24a0fd0320487483d72c43972401fe64bea
RodSoriano <rsoriano.jose@gmail.com>
2023-07-30 23:39:44 -0600

    Create spanish language files.

---
+++ lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Hemos enviado el enlace para restablecer tu contraseña por correo electrónico!',
+    'throttled' => 'Por favor, espera antes de intentarlo de nuevo.',
+    'token' => 'Este token para restablecer la contraseña no es válido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+];
