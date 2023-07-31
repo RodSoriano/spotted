d434d50127222eeb6b4824392c9f74d582057f6b
RodSoriano <rsoriano.jose@gmail.com>
2023-07-31 02:53:52 -0600

    Update index page translated text keys.

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -11,16 +11,16 @@ const Index = ({ localeText, message }) => {
     <>
       {message && <Alert message={message} />}
 
-      <CustomLink hrefProp={'/register'} message={localeText.buttons.register} />
+      <CustomLink hrefProp={'/register'} message={localeText.register} />
 
-      <CustomLink color={'bg-purple-500'} hrefProp={'/reservation'} message={localeText.buttons.book} />
+      <CustomLink color={'bg-purple-500'} hrefProp={'/reservation'} message={localeText.book} />
 
-      <CustomLink color={'bg-green-500'} hrefProp={'/check-in'} message={localeText.buttons.checkIn} />
+      <CustomLink color={'bg-green-500'} hrefProp={'/check-in'} message={localeText.checkIn} />
 
       <div className='flex items-center'>
         <p>
-          {localeText.localeSwitch}
-          <Link className='text-blue-500 underline' href='/language'> {localeText.clickHere} </Link>
+          {localeText.footer}
+          <Link className='text-blue-500 underline' href='/language'> {localeText.click} </Link>
         </p>
       </div>
     </>
