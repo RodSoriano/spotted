95160e94b604a033a0e88db37ded501c87667767
RodSoriano <rsoriano.jose@gmail.com>
2023-07-31 01:58:23 -0600

    Create index controller to pass translated text to view.

---
+++ app/Http/Controllers/Web/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\Locale\LocaleTextPageSelector;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class IndexController extends Controller
+{
+    use LocaleTextPageSelector;
+
+    public function show(): Response
+    {
+        return Inertia::render('Index', [
+            'localeText' => $this->indexText(),
+        ]);
+    }
+}
