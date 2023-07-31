f5124ee70dde3aa9c5defcd9e275a26e4373a4e5
RodSoriano <rsoriano.jose@gmail.com>
2023-07-31 02:56:10 -0600

    Update register controller to pass translated text to view.

--- app/Http/Controllers/Web/RegisterController.php
+++ app/Http/Controllers/Web/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\Locale\LocaleTextPageSelector;
 use App\Http\Requests\StoreUserRequest;
 use App\Services\User\UserCreator;
 use Inertia\Inertia;
@@ -10,9 +11,13 @@ use Inertia\Response;
 
 class RegisterController extends Controller
 {
+    use LocaleTextPageSelector;
+
     public function toForm(): Response
     {
-        return Inertia::render('User/Register');
+        return Inertia::render('User/Register', [
+            'localeText' => $this->registerText(),
+        ]);
     }
 
     public function store(StoreUserRequest $request, UserCreator $userService): Response
@@ -23,6 +28,7 @@ class RegisterController extends Controller
 
         return Inertia::render('Index', [
             'message' => $response['message'],
+            'localeText' => $this->indexText(),
         ]);
     }
 }
