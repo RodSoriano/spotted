c2d1180a4386a99cbc2909f096bf121bed86aee6
RodSoriano <rsoriano.jose@gmail.com>
2023-07-31 19:14:58 -0600

    Create messages.php for 'en' and 'es'.

---
+++ lang/en/messages.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'alerts' => [
+        'reservation' => [
+            'success' => 'Your reservation has been created.',
+            'error' => 'Sorry, no spots are left, try another date.',
+        ],
+    ],
+
+    'titles' => [
+        'welcome' => 'Welcome to the Park',
+        'book' => 'Create a Reservation',
+        'daypass' => 'Great to See You Again!',
+        'checkIn' => 'Register Your Entrance',
+    ],
+
+    'statements' => [
+        'register' => 'Please enter your information to register.',
+        'checkIn' => 'Enter the email you used to make your reservation.',
+    ],
+
+    'footers' => [
+        'language' => 'Want to switch languages?',
+        'click' => 'Click Here!',
+        'account' => 'Already have an account?',
+        'book' => 'Book a spot!',
+        'accountYet' => 'Don\'t have an account yet?',
+        'register' => 'Register!',
+
+        'checkIn' => [
+            'footer' => 'Don\'t have a reservation yet?',
+            'toBook' => 'Book a spot!',
+        ],
+    ],
+
+    'buttons' => [
+        'register' => 'Register',
+        'book' => 'Book a Space',
+        'check' => 'Check In',
+        'join' => 'Join',
+        'reservation' => 'Make Reservation',
+        'language' => 'Save',
+    ],
+
+    'labels' => [
+        'firstName' => 'First Name',
+        'lastName' => 'Last Name',
+        'email' => 'Email Address',
+        'birthDate' => 'Date of Birth',
+        'contactName' => 'Emergency Contact Name',
+        'contactNumber' => 'Emergency Contact Phone',
+        'picture' => 'Upload Your Picture',
+
+        'book' => [
+            'email' => 'Enter your email account',
+            'date' => 'Pick a Date',
+        ],
+    ],
+
+    'redirect' => [
+        'register' => [
+            'success' => 'Your user has been created.',
+            'error' => 'Sorry, something went wrong, try again.',
+        ],
+    ],
+];
---
+++ lang/es/messages.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'alerts' => [
+        'reservation' => [
+            'success' => 'Su reservación ha sido creada.',
+            'error' => 'Lo sentimos, no hay lugares disponibles, intente otra fecha.',
+        ],
+    ],
+
+    'titles' => [
+        'welcome' => 'Bienvenido al Parque',
+        'book' => 'Crea una Reservación',
+        'daypass' => '¡Qué bueno verte de nuevo!',
+        'checkIn' => 'Registra tu entrada',
+    ],
+
+    'statements' => [
+        'register' => 'Por favor, ingresa tu información para registrarte.',
+        'checkIn' => 'Ingresa el correo electrónico que utilizaste para hacer tu reserva.',
+    ],
+
+    'footers' => [
+        'language' => '¿Quieres cambiar de idioma?',
+        'click' => 'Haz clic aquí!',
+        'account' => '¿Ya tienes una cuenta?',
+        'book' => 'Reserva un lugar!',
+        'accountYet' => '¿Todavía no tienes una cuenta?',
+        'register' => 'Registrarse!',
+
+        'checkIn' => [
+            'footer' => '¿Todavía no tienes una reservación?',
+            'toBook' => '¡Reserva tu lugar!',
+        ],
+    ],
+
+    'buttons' => [
+        'register' => 'Registrarse',
+        'book' => 'Reservar',
+        'check' => 'Check In',
+        'join' => 'Unirse',
+        'reservation' => 'Hacer Reservación',
+        'language' => 'Guardar',
+    ],
+
+    'labels' => [
+        'firstName' => 'Nombre',
+        'lastName' => 'Apellido',
+        'email' => 'Correo Electrónico',
+        'birthDate' => 'Fecha de Nacimiento',
+        'contactName' => 'Nombre de Contacto de Emergencia',
+        'contactNumber' => 'Teléfono de Contacto de Emergencia',
+        'picture' => 'Sube tu Foto',
+
+        'book' => [
+            'email' => 'Ingresa el Correo de tu Cuenta',
+            'date' => 'Selecciona una Fecha',
+        ],
+    ],
+
+    'redirect' => [
+        'register' => [
+            'success' => 'Your user has been created.',
+            'error' => 'Lo siento, algo salió mal. Inténtalo de nuevo.',
+        ],
+    ],
+];
