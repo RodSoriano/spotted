54299373a4a46260d40b6c56c6d23c6cd95bcce8
RodSoriano <rsoriano.jose@gmail.com>
2023-08-02 03:38:23 -0600

    Update component styling to fit in phone screen.

--- resources/js/Pages/Popups/TermsAndConditions.jsx
+++ resources/js/Pages/Popups/TermsAndConditions.jsx
@@ -8,9 +8,9 @@ import 'reactjs-popup/dist/index.css';
 const TermsAndConditions = ({ onAccept, onDecline, localeText }) => {
   const styles = {
     popupBox: 'fixed inset-0 flex items-center justify-center bg-black bg-opacity-50',
-    box: 'w-2/3 bg-white rounded-md p-6 border border-gray-300 text-center',
+    box: 'w-3/4 bg-white rounded-md p-6 border border-gray-300 text-center',
     heading: 'text-2xl font-bold mb-4',
-    content: 'text-lg mb-4',
+    content: 'text-m mb-4',
     buttonsContainer: 'flex justify-center',
   };
 
