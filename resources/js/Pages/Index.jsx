9a9a78d73fa4d587200d19f919ed43aa3e8221a4
RodSoriano <rsoriano.jose@gmail.com>
2023-08-08 23:52:30 -0600

    Update index component with proper colors.

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import Layout from './Layout';
 
-import CustomLink from '../components/CustomLink';
 import Alert from '../components/Alert';
+import CustomLink from '../components/CustomLink';
 import FooterLink from '../components/FooterLink';
 
 const Index = ({ localeText, message }) => {
-
   return (
     <>
       {message && <Alert message={message} />}
 
       <CustomLink hrefProp={'/register'} message={localeText.register} />
 
-      <CustomLink color={'bg-purple-500'} hrefProp={'/reservation'} message={localeText.book} />
+      <CustomLink color={'bg-purple-600'} hrefProp={'/reservation'} message={localeText.book} />
 
-      <CustomLink color={'bg-green-500'} hrefProp={'/check-in'} message={localeText.checkIn} />
+      <CustomLink color={'bg-stone-700'} hrefProp={'/check-in'} message={localeText.checkIn} />
 
       <FooterLink text={localeText.footer} link={'/language'} click={localeText.click} />
     </>
