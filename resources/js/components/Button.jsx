eb5392b4cb2b4a123d6c1654599ce1feb7afd888
RodSoriano <rsoriano.jose@gmail.com>
2023-08-09 00:41:26 -0600

    Update button component with proper colors and shape.

--- resources/js/components/Button.jsx
+++ resources/js/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = React.forwardRef(({ type, message, color, onClick }, innerRef) => {
-  const buttonClass = `${color} text-white py-2 px-4 rounded-full font-bold text-lg mb-4 mx-2 transform transition-transform hover:scale-105`;
+  const buttonClass = `${color} text-white py-2 px-4 rounded-md font-bold text-lg mb-4 mx-2 transform transition-transform hover:scale-105`;
 
   return (
     <button className={buttonClass} type={type} onClick={onClick}>
@@ -13,7 +13,7 @@ const Button = React.forwardRef(({ type, message, color, onClick }, innerRef) =>
 Button.defaultProps = {
   type: 'button',
   message: '',
-  color: 'bg-blue-500',
+  color: 'bg-teal-400',
   onClick: () => { },
 };
 
