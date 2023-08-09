8456bb5744fefebab4dc5b25945ab052bbda3646
RodSoriano <rsoriano.jose@gmail.com>
2023-08-09 02:14:40 -0600

    Update language page with proper styles and colors.

--- resources/js/Pages/User/Language.jsx
+++ resources/js/Pages/User/Language.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Layout from '../Layout';
 import { Inertia } from '@inertiajs/inertia';
+
 import Button from '../../components/Button';
 import Title from '../../components/Title';
 
@@ -10,7 +11,7 @@ const Language = ({ localeText }) => {
   let textPosition = language === 'es' ? 'justify-end' : 'justify-left';
 
   const styles = {
-    toggle: `flex items-center ${textPosition} px-1  mb-4 w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-white rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600`
+    toggle: `flex items-center ${textPosition} px-1 w-11 h-8 bg-stone-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-white rounded-sm peer dark:bg-stone-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-sm after:h-7 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-500`
   };
 
   const handleLanguageChange = (e) => {
@@ -37,7 +38,9 @@ const Language = ({ localeText }) => {
         </div>
       </label>
 
-      <Button onClick={saveLanguage} message={localeText.button} />
+      <div className='mt-4'>
+        <Button onClick={saveLanguage} message={localeText.button} />
+      </div>
     </>
   );
 };
