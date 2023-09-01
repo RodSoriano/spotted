5ab91a75f5acc469ecb40199084a80801483fb72
RodSoriano <rsoriano.jose@gmail.com>
2023-09-01 02:09:06 -0600

    Update cron job runner to send programmed mails.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,18 +2,29 @@
 
 namespace App\Console;
 
+use App\Mail\ReservationsProcessed;
 use App\Models\Reservation;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Mail;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
         $schedule->call(function () {
-            Reservation::where('is_done', 0)
+            $reservations = Reservation::query();
+            $users = Reservation::latest()->first()->users()->get();
+
+            $reservations->where('is_done', 0)
                 ->where('date', date('Y-m-d'))
                 ->update(['is_done' => 1]);
+
+            if ($users->all()) {
+                Mail::to(env('DEVELOPMENT_EMAIL'))->locale('es')->send(
+                    new ReservationsProcessed($users)
+                );
+            }
         });
     }
 
